4a76e37f188dfb0a78ad1b551df264d61c9ebf22
Hasan Hazbi <showkathazbi99@gmail.com>
2026-05-22 12:36:52 +0600

    Update budget thingy.py
    ADDED percentage values to be inputed via users, so users can select their own percentage. To evaluate if the percentages are matching perfectly, an evaluating function will be added in future.

--- budget thingy.py
+++ budget thingy.py
@@ -1,17 +1,22 @@
 Salary = int(input("Input your salary(Intership money) : "))#PaidInternship
+currency = input("Select currency : ")
+
 pit1 = input("Majority expenses(Just type the name) : ")
+percent1 = int(input("""Put majority percentage(Just put the integer value, no "%" sign needed) : """))
 pit2 = input("Other expenses : ")
+percent2 = int(input("""Put the expenses percentage(Just put the integer value, no "%" sign needed) : """))
 pit3 = input("Savings : ")
+percent3 = int(input("""Put savings percentage(Just put the integer value, no "%" sign needed) : """))
 
 budgets = [pit1, pit2, pit3]
 
-percentage = [Salary * .70, Salary * .20, Salary * .10]
+percentage = [Salary * percent1 / 100, Salary * percent2 / 100, Salary * percent3 / 100]
 
-print(f"""Monthly salary - {Salary} taka
+print(f"""Monthly salary - {Salary} {currency}
 
-{budgets[0]} - {percentage[0]} taka
-{budgets[1]} - {percentage[1]} taka
-{budgets[2]} - {percentage[2]} taka""")
+{budgets[0]} - {percentage[0]} {currency}
+{budgets[1]} - {percentage[1]} {currency}
+{budgets[2]} - {percentage[2]} {currency}""")
 
 
 
