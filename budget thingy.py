a10076104f40d718f6e35cf550bba629fcfdd9ab
Hasan Hazbi <showkathazbi99@gmail.com>
2026-05-30 15:52:50 +0600

    Update budget thingy.py
    Removed "(internship money)" text from asking.

--- budget thingy.py
+++ budget thingy.py
@@ -1,4 +1,4 @@
-Salary = int(input("Input your salary(Intership money) : "))#PaidInternship
+Salary = int(input("Input your salary : "))#PaidInternship
 currency = input("Select currency : ")
 
 pit1 = input("Majority expenses(Just type the name) : ")
